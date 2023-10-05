4ad085753397b68917d249c80642dd499376d0a0
Kay Schnyder <schnyderkay@gmail.com>
2023-10-05 11:17:31 +0200

    init

---
+++ BookList/TestProject/UnitTest.cs
@@ -0,0 +1,58 @@
+namespace TestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestAddBook()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestRemoveBook()
+        {
+
+        }
+
+        [Test]
+        public void TestGetBook()
+        {
+
+        }
+
+        [Test]
+        public void TestChangeReadStatus()
+        {
+
+        }
+
+        [Test]
+        public void TestGetBooklist()
+        {
+
+        }
+
+        [Test]
+        public void TestAddList()
+        {
+
+        }
+
+        [Test]
+        public void TestChangeListName()
+        {
+
+        }
+
+        [Test]
+        public void TestRemoveList() 
+        {
+
+        }
+    }
+}
