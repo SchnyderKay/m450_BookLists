62d9901934d0381c70391485a4a785122e61073c
Kay Schnyder <kay.schnyder@swissphone.com>
2023-10-30 15:51:25 +0100

    added tests

--- BookList/TestProject/UnitTest.cs
+++ BookList/TestProject/UnitTest.cs
@@ -40,6 +40,10 @@ namespace TestProject
             Book result = library.GetBook("testBook");
 
             Assert.AreEqual(expected.GetName(), result.GetName());
+            Assert.AreEqual(expected.GetAuthor(), result.GetAuthor());
+            Assert.AreEqual(expected.GetPages(), result.GetPages());
+            Assert.AreEqual(expected.GetPublishedYear(), result.GetPublishedYear());
+            Assert.AreEqual(expected.GetReadingStatus(), result.GetReadingStatus());
         }
 
         [Test]
@@ -48,17 +52,22 @@ namespace TestProject
             List<Book> expected = new();
             expected.Add(new Book("book1", "author1", 1999, 1, "Reading"));
             expected.Add(new Book("book2", "author2", 1999, 1, "Unknown"));
-            expected.Add(new Book("book3", "author3", 1999, 1, "Finsihed"));
+            expected.Add(new Book("book3", "author3", 1999, 1, "Finished"));
             expected.Add(new Book("book4", "author4", 1999, 1, "Dropped"));
 
             library.NewBookList("test", "author");
 
             library.AddBook("test", "book1", "author1", 1999, 1, "Reading");
             library.AddBook("test", "book2", "author2", 1999, 1, "Unknown");
-            library.AddBook("test", "book3", "author3", 1999, 1, "Finsihed");
+            library.AddBook("test", "book3", "author3", 1999, 1, "Finished");
             library.AddBook("test", "book4", "author4", 1999, 1, "Dropped");
 
-            List<Book> result = library.GetBookList("test").GetBooks();
+            List<Book> results = library.GetBookList("test").GetBooks();
+
+            foreach (Book result in results)
+            {
+                Assert.True(expected.Where(x => x.GetName() == result.GetName()).Count() == 1);
+            }
         }
 
         [Test]
@@ -75,11 +84,53 @@ namespace TestProject
 
 
         [Test]
-        public void TestRemoveList() 
+        public void TestRemoveList()
         {
             var result = library.RemoveBookList("testList");
 
             Assert.True(result);
+        }
+
+        [Test]
+        public void TestGetLists()
+        {
+            library = new Library();
+
+            List<BookList> expected = new List<BookList>();
+            expected.Add(new BookList("test1", "user1"));
+            expected.Add(new BookList("test2", "user2"));
+            expected.Add(new BookList("test3", "user3"));
+
+            library.NewBookList("test1", "user1");
+            library.NewBookList("test2", "user2");
+            library.NewBookList("test3", "user3");
+
+            List<BookList> results = library.GetBookLists();
+
+            foreach (BookList result in results)
+            {
+                Assert.True(expected.Where(x => x.GetName() == result.GetName()).Count() == 1);
+            }
+        }
+
+        [Test]
+        public void TestChangeListName()
+        {
+            var result = library.ChangeListName("testList", "test1");
+
+            Assert.True(result);
+        }
+
+
+        [Test]
+        public void TestChangeReadStatus()
+        {
+            string testStatus = "Dropped";
+
+            library.AddBook("testList", "test1", "admin", 157, 1999, "Finished");
+            library.ChangeReadStatus(testStatus, "testList", "test1");
+
+            Assert.AreEqual(testStatus, library.GetBook("test1").GetReadingStatus());
         }
     }
 }
